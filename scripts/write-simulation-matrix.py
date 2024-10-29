b24bbe19ac396c5cefdb95f7a5989383272eceb5
Jiangyue-Wang <lily_wang@pku.edu.cn>
2024-10-28 21:20:55 -0700

    I added a graph plotting line, but it seems that the matrix does not represent the graph in google doc?

--- scripts/write-simulation-matrix.py
+++ scripts/write-simulation-matrix.py
@@ -5,6 +5,7 @@
 # import network package
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Create a simulation adjacency matrix
 # This matrix has 6 individuals, one of which has no connections.
@@ -22,7 +23,7 @@ matrix0 = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-^			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
 			[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
 			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
@@ -34,3 +35,5 @@ G = nx.from_numpy_array(matrix0)
 
 # Print the edges of the graph
 print(G.edges())
+nx.draw(G, with_labels=True, node_color="skyblue", node_size=1500, font_size=16, font_color="black")
+plt.show()
